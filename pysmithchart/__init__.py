6c7f0ff7e7e919ae54007aa6b376103ffd11ee27
Scott Prahl <scott@prahls.com>
2025-01-25 22:03:38 -0800

    add docstring

--- pysmithchart/__init__.py
+++ pysmithchart/__init__.py
@@ -1,3 +1,33 @@
+"""
+This module registers the matplotlib projection "smith".
+
+The `pysmithchart` package provides tools for working with Smith charts, enabling visualization and
+analysis of reflection coefficients, impedances, and admittances in RF and microwave engineering.
+
+Modules:
+    smithaxes: Defines the `SmithAxes` class, which implements the custom Smith chart projection.
+
+Constants:
+    S_PARAMETER: Scattering parameter (S-parameter) type, used for plotting reflection coefficients.
+    Z_PARAMETER: Impedance parameter (Z-parameter) type, used for plotting normalized impedances.
+    Y_PARAMETER: Admittance parameter (Y-parameter) type, used for plotting normalized admittances.
+
+Public API:
+    - SmithAxes: The custom projection class for Smith charts.
+    - S_PARAMETER: Constant for S-parameter plotting.
+    - Z_PARAMETER: Constant for Z-parameter plotting.
+    - Y_PARAMETER: Constant for Y-parameter plotting.
+
+Example:
+    Import the module and plot a reflection coefficient using the Smith chart projection:
+
+    >>> import matplotlib.pyplot as plt
+    >>> from pysmithchart import Z_PARAMETER
+    >>> plt.subplot(1, 1, 1, projection="smith")
+    >>> plt.plot([0.5 + 0.3j, -0.2 - 0.1j], 'o', datatype=Z_PARAMETER)
+    >>> plt.show()
+"""
+
 from matplotlib.projections import register_projection
 from .smithaxes import SmithAxes, S_PARAMETER, Z_PARAMETER, Y_PARAMETER
 
